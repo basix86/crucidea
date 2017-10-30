04a2d1f74ca12600fd09c9b11bcd96c2be8387ab
mmatessi <matessi@esteco.com>
2017-10-30 17:16:21 +0100

    ideaRefactor

--- main.js
+++ main.js
@@ -3,22 +3,24 @@
 const electron = require('electron');
 
 const {app, Menu, Tray, BrowserWindow, ipcMain, shell} = require('electron');
-var mainWindow = null;
-let tray = null
 
-var fs = require('fs');
-var http = require('http');
-var url = require('url');
-var xml2js = require('xml2js');
-var path = require('path');
-var dialog = require('electron').dialog
-var serverAddress = '127.0.0.1';
-var serverPort = 51235;
-var iconPath = './icon.png';
-var iconFile = fs.readFileSync(iconPath);
-var homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-var openIdeaCommand = "idea";
-var server;
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const xml2js = require('xml2js');
+const path = require('path');
+const dialog = require('electron').dialog;
+const serverAddress = '127.0.0.1';
+const serverPort = 51235;
+const iconPath = './icon.png';
+const iconFile = fs.readFileSync(iconPath);
+const homePath = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+const openIdeaCommand = "idea";
+
+let mainWindow = null;
+let tray = null
+let server;
+let projectPath;
 
 app.on('ready', function () {
 
@@ -34,13 +36,13 @@ app.on('ready', function () {
     const contextMenu = Menu.buildFromTemplate([
         { label: 'Settings', click() { openSettings() } },
         { label: 'Close', click() { closeApplication() } }
-    ])
+    ]);
     tray.setToolTip('This is my application.');
     tray.setContextMenu(contextMenu);
 
     let recentProjectsXmlPaths = getIdeaRecentProjectsFilePaths();
     let lastProjectsXmlPath = recentProjectsXmlPaths[recentProjectsXmlPaths.length - 1];
-    let projectPath = getOpenPath(lastProjectsXmlPath);
+    this.projectPath = getOpenPath(lastProjectsXmlPath);
 
     server = createServer(projectPath);
     server.listen(serverPort, serverAddress);
@@ -52,8 +54,8 @@ app.on('ready', function () {
 
 function createServer(projectPath) {
     return http.createServer(function (req, res) {
-        var request = url.parse(req.url, true);
-        var queryReq = request.query;
+        const request = url.parse(req.url, true);
+        const queryReq = request.query;
 
         if (queryReq.file && queryReq.line) {
             let filePath = projectPath + "/" + queryReq.file;
@@ -72,17 +74,17 @@ function createServer(projectPath) {
 
 function getOpenPath(filePath) {
     if (fs.existsSync(filePath)) {
-        var projectPath;
+        let projectPath;
         try {
-            var fileData = fs.readFileSync(filePath, 'ascii');
-            var parser = xml2js.Parser({
+            const fileData = fs.readFileSync(filePath, 'ascii');
+            const parser = xml2js.Parser({
                 explicitArray: true
             });
 
             parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
-                var json = JSON.stringify(result);
-                var configuration = JSON.parse(json);
-                var parsedPath = configuration.application.component[0].option[1].list[0].option[0].$.value;
+                const json = JSON.stringify(result);
+                const configuration = JSON.parse(json);
+                const parsedPath = configuration.application.component[0].option[1].list[0].option[0].$.value;
                 projectPath = parsedPath.replace("$USER_HOME$", homePath);
             });
             console.log("Open path = " + projectPath + " was successfully read.");
@@ -100,28 +102,28 @@ function openSettings() {
     let win = new BrowserWindow({ width: 400, height: 320 });
     win.on('closed', () => { win = null });
     win.setMenu(null);
-    win.loadURL(modalPath)
+    win.loadURL(modalPath);
     win.show()
 }
 
 function handleSubmission() {
     ipcMain.on('save-settings', (event, argument) => {
         const { xmlPath } = argument;
-        recentProjectsXmlPath = xmlPath;
-        projectPath = getOpenPath(recentProjectsXmlPath);
+        const recentProjectsXmlPath = xmlPath;
+        this.projectPath = getOpenPath(recentProjectsXmlPath);
 
         console.log(recentProjectsXmlPath);
     });
 
     ipcMain.on('restore-defaults', (event, argument) => {
         console.log("restore");
-        var ideaRecentProjectsFilePaths = getIdeaRecentProjectsFilePaths().sort();
-        var lastRecentProjectsFilePath = ideaRecentProjectsFilePaths[ideaRecentProjectsFilePaths.length - 1];
+        let ideaRecentProjectsFilePaths = getIdeaRecentProjectsFilePaths().sort();
+        let lastRecentProjectsFilePath = ideaRecentProjectsFilePaths[ideaRecentProjectsFilePaths.length - 1];
         event.sender.send('set-recent-project-path', lastRecentProjectsFilePath);
     });
 
     ipcMain.on('open-folder-page', (event, argument) => {
-        var settingUrl = "https://intellij-support.jetbrains.com/hc/en-us/articles/206544519-Directories-used-by-the-IDE-to-store-settings-caches-plugins-and-logs";
+        let settingUrl = "https://intellij-support.jetbrains.com/hc/en-us/articles/206544519-Directories-used-by-the-IDE-to-store-settings-caches-plugins-and-logs";
         shell.openExternal(settingUrl);
     });
 }
@@ -138,7 +140,7 @@ function getIdeaRecentProjectsFilePaths() {
     }).filter(function (file) {
         return fs.statSync(file).isDirectory();
     }).filter(function (file) {
-        return path.basename(file).startsWith(".IdeaIC");
+        return path.basename(file).startsWith(".IdeaIC") || path.basename(file).startsWith(".IntelliJIdea");
     }).map(function (file) {
         return path.join(file, "config/options/recentProjects.xml");
     }).filter(function (recentProjectsFilePath) {
